1f88800f26b314c6ce5f037205b756b49ae405e8
Mihai <mihaivirlan1@mail.ru>
2017-09-03 12:52:39 +0300

    adminca

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,12 +1,5 @@
-    <!-- Bootstrap core CSS -->
-    <link href="{{asset('public/css/app.css')}}" rel="stylesheet">
-    <link href="{{asset('public/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="{{asset('public/css/clean-blog.css')}}" rel="stylesheet">
-
-@include('layouts.navbar')
-
+@extends('layouts.app')
+@section('content')
     <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
             @forelse($posts as $postare)
@@ -30,7 +23,4 @@
                 {{ $posts->links() }}
         </div>
     </div>
-
-    @yield('content')
-
-    @include('layouts.footer')
+@endsection
