4a478205180e066fba7422feecb6aad661ece58f
Mihai <mihaivirlan1@mail.ru>
2017-09-03 13:13:16 +0300

    adminca

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,16 +1,6 @@
-    <!-- Bootstrap core CSS -->
-    <link href="{{asset('public/css/app.css')}}" rel="stylesheet">
-    <link href="{{asset('public/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+@extends('layouts.app')
 
-    <!-- Custom fonts for this template -->
-    <link href="{{asset('public/vendor/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
-    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-    <!-- Custom styles for this template -->
-    <link href="{{asset('public/css/clean-blog.css')}}" rel="stylesheet">
-
-@include('layouts.navbar')
+@section('content')
 
     <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
@@ -60,8 +50,7 @@
             </form>
         </div>
     </div>
-
-@include('layouts.footer')
+@endsection
 
 
 
